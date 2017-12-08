d7744ab9a19f4d1f1d8c7177c62471ad2e1c0fe7
Michael Burge <michaelburge@pobox.com>
2017-12-07 22:55:45 -0800

    accesses-memory? checks for memory access instructions now

--- macro.rkt
+++ macro.rkt
@@ -4,14 +4,31 @@
 (require "ast.rkt")
 (require "compiler.rkt")
 (require "codegen.rkt")
+(require "io.rkt")
 
 (provide (all-defined-out))
-
 #|
 This module is required into the namespace used to evaluate Pyramid macros.
 
-Functions defined here are available to Pyramid programs.
+Functions defined here are available to Pyramid programs within macros.
 |#
 
+; Compiles a fragment of code rather than a whole program.
+; The fragment doesn't have standard library or environment initialization code.
+(: minicompile (-> Pyramid EthInstructions))
+(define (minicompile prog)
+  (codegen-list (inst-seq-statements (compile-pyramid prog 'val 'next))))
+
+(: contains-instruction? (-> Pyramid (-> EthInstruction Boolean) Boolean))
+(define (contains-instruction? prog pred)
+  (let ((is (minicompile prog)))
+    (ormap pred is)))
+
 (: accesses-memory? (-> Pyramid Boolean))
-(define (accesses-memory? exp) #f)
+(define (accesses-memory? exp)
+  (define (read? i) (equal? i (eth-asm 'MLOAD)))
+  (define (write? i) (equal? i (eth-asm 'MSTORE)))
+  (define (inst-pred i) (or (read? i)
+                            (write? i)))
+  (contains-instruction? exp inst-pred))
+
